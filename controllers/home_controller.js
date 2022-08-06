696391c2f48925feb444d35b7a042f09c7129a70
rajeshRoul <roul.rajesh28@gmail.com>
2022-08-06 14:09:20 +0530

    Setup: Frirst Controller for Home Page

---
+++ controllers/home_controller.js
@@ -0,0 +1,3 @@
+module.exports.home = (req, res) => {
+  res.end("<h1>Home Page</h1>");
+};
--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,8 @@
 const express = require("express");
+const HomeController = require("../controllers/home_controller");
 
 const router = express.Router();
+
+router.get("/", HomeController.home);
 
 module.exports = router;
