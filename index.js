1227ef5b7f4f2b8a918da7120c21f06597f66b83
rajeshRoul <roul.rajesh28@gmail.com>
2022-08-06 13:48:33 +0530

    Setup: Express Router

--- index.js
+++ index.js
@@ -3,6 +3,9 @@ const express = require("express");
 const app = express();
 const port = 8000;
 
+// Use Express Router
+app.use("/", require("./routes"));
+
 app.listen(port, (err) => {
   if (err) {
     return console.log("Error in running Server", port);
